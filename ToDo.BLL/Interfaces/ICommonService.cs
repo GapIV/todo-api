9060deaf0b72e862a70a26c603d731589e0bef80
GapIV <danichgap@gmail.com>
2024-02-12 17:41:50 +0300

    ICommonService interface added

---
+++ ToDo.BLL/Interfaces/ICommonService.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.BLL.Interfaces;
+
+public interface ICommonService<T> where T : class
+{
+    Task<T> Get(Guid id);
+    Task<T> GetAll();
+    Task<T> Create(T item);
+    Task<T> Update(T item);
+    Task Delete(Guid id);
+}
