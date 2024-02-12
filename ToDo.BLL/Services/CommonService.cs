4aa184a8b46fa23b7778ccd69d3f1de2e8366c81
GapIV <danichgap@gmail.com>
2024-02-12 18:00:02 +0300

    CommonService added

---
+++ ToDo.BLL/Services/CommonService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.BLL.Interfaces;
+
+namespace ToDo.BLL.Services;
+
+public class CommonService<T> : ICommonService<T> where T : class
+{
+    public Task<T> Create(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> Update(T item)
+    {
+        throw new NotImplementedException();
+    }
+}
