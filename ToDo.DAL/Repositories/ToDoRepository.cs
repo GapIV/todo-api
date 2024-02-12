632083c1d34f518f0e4f410b8f9bbd77c121c784
GapIV <danichgap@gmail.com>
2024-02-12 16:44:18 +0300

    ToDoRepository added

---
+++ ToDo.DAL/Repositories/ToDoRepository.cs
@@ -0,0 +1,8 @@
+﻿using ToDo.DAL.Entities;
+
+namespace ToDo.DAL.Repositories;
+
+public class ToDoRepository : CommonRepository<ToDoEntity>
+{
+    public ToDoRepository(ApplicationContext context) : base(context) { }
+}
