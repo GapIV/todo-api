9dc50a4518faac86cb668f3641c264d77ae0c356
GapIV <danichgap@gmail.com>
2024-02-12 17:51:34 +0300

    ToDo model created

---
+++ ToDo.BLL/Models/ToDoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.BLL.Models;
+
+public class ToDoModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Body { get; set; }
+    public bool Checked { get; set; }
+}
