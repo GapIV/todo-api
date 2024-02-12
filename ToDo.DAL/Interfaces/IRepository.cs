62352b5905e0cc278a9d5d56a9e1722e6fc2fb59
GapIV <danichgap@gmail.com>
2024-02-12 15:57:02 +0300

    IRepository interface added

---
+++ ToDo.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.DAL.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> Get(Guid id);
+    Task<IEnumerable<T>> GetAll();
+    Task<T> Create(T entity);
+    Task<T> Update(T entity);
+    Task Delete(Guid id);
+}
