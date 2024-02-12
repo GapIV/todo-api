e18d395fe88f9133c9b4a04113ab134c67937436
GapIV <danichgap@gmail.com>
2024-02-12 18:34:01 +0300

    ICommonService updated

--- ToDo.BLL/Interfaces/ICommonService.cs
+++ ToDo.BLL/Interfaces/ICommonService.cs
@@ -3,7 +3,7 @@
 public interface ICommonService<T> where T : class
 {
     Task<T> Get(Guid id);
-    Task<T> GetAll();
+    Task<IEnumerable<T>> GetAll();
     Task<T> Create(T model);
     Task<T> Update(T model);
     Task Delete(Guid id);
