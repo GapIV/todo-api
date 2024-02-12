6845d2265a7dd70f48531bae8e9fbae666dc926b
GapIV <danichgap@gmail.com>
2024-02-12 15:47:38 +0300

    Application data context been created

---
+++ ToDo.DAL/Entities/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.DAL.Entities;
+
+public class AppContext : DbContext
+{
+    DbSet<ToDo> ToDos { get; set; }
+
+    public AppContext() {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("host=localhost; port=5432; database=ToDo; username=postgres; password=postgres");
+    }
+}
