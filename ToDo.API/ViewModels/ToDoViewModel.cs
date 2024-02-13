e7f646a9651f2dfe087bb729fc3310ac5d1a75f4
GapIV <danichgap@gmail.com>
2024-02-13 14:48:33 +0300

    ToDoViewModel added

---
+++ ToDo.API/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.API.ViewModels;
+
+public class ToDoViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Body { get; set; }
+    public bool Checked { get; set; }
+}
