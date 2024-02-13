5eb3ca0c630848f6f0ab985193fe2083db1444ba
GapIV <danichgap@gmail.com>
2024-02-13 15:19:20 +0300

    MappingProfile added to API layer

---
+++ ToDo.API/Mapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ToDo.API.ViewModels;
+using ToDo.BLL.Models;
+
+namespace ToDo.API.Mapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ToDoModel, ToDoViewModel>().ReverseMap();
+    }
+}
