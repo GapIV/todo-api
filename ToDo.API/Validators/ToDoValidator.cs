b57935e401ff63e4abd98301555a3d815222502e
GapIV <danichgap@gmail.com>
2024-02-13 17:49:09 +0300

    ToDoValidator added

---
+++ ToDo.API/Validators/ToDoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ToDo.API.ViewModels;
+
+namespace ToDo.API.Validators;
+
+public class ToDoValidator : AbstractValidator<ToDoViewModel>
+{
+    public ToDoValidator()
+    {
+        RuleFor(e => e.Title)
+            .MaximumLength(50)
+            .NotEmpty();
+        RuleFor(e => e.Body)
+            .MaximumLength(150);
+    }
+}
