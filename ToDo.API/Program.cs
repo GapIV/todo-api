b3a68778b4b3ff48cbe7201a2a17b1e86db71455
GapIV <danichgap@gmail.com>
2024-02-13 15:06:43 +0300

    program end-point added

---
+++ ToDo.API/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using ToDo.BLL.DI;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddControllers();
+
+builder.Services.AddBusinessLogic(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
