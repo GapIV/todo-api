40efa626f3ace80be2ae2b7a8de32ce1a5cb122f
GapIV <danichgap@gmail.com>
2024-02-14 14:40:25 +0300

    ToDoRepository updated

--- ToDo.DAL/Repositories/ToDoRepository.cs
+++ ToDo.DAL/Repositories/ToDoRepository.cs
@@ -1,8 +1,21 @@
-﻿using ToDo.DAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.DAL.Entities;
 
 namespace ToDo.DAL.Repositories;
 
 public class ToDoRepository : CommonRepository<ToDoEntity>
 {
-    public ToDoRepository(ApplicationContext context) : base(context) { }
+    public ToDoRepository(ApplicationContext context) : base(context) 
+    {
+    }
+
+    public override async Task<ToDoEntity?> Get(Guid id)
+    {
+        return await _set.Include(e => e.User).FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public override async Task<IEnumerable<ToDoEntity>> GetAll()
+    {
+        return await _set.Include(e => e.User).ToListAsync();
+    }
 }
