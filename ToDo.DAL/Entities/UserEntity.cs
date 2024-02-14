a9bea3d1fd62033fa778c9f724422685999cf043
GapIV <danichgap@gmail.com>
2024-02-14 14:14:45 +0300

    UserEntity added

---
+++ ToDo.DAL/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.DAL.Entities;
+
+public class UserEntity : CommonEntity
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string? Email { get; set; }
+
+    public List<ToDoEntity> ToDos { get; set; } = new List<ToDoEntity>();
+}
