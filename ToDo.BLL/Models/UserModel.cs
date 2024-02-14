f6a774f4cb318671a03e1ab574f690601a49d748
GapIV <danichgap@gmail.com>
2024-02-14 14:25:20 +0300

    UserModel updated

--- ToDo.BLL/Models/UserModel.cs
+++ ToDo.BLL/Models/UserModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ToDo.BLL.Models;
 
-namespace ToDo.BLL.Models;
 public class UserModel
 {
     public Guid Id { get; set; }
