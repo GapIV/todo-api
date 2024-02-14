131a1fbdb2307acba1665ac1049833bb69a8fabb
GapIV <danichgap@gmail.com>
2024-02-14 14:41:47 +0300

    MappingProfile extended with User map

--- ToDo.BLL/Mapper/MappingProfile.cs
+++ ToDo.BLL/Mapper/MappingProfile.cs
@@ -10,5 +10,6 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<ToDoEntity, ToDoModel>().ReverseMap();
+        CreateMap<UserEntity, UserModel>().ReverseMap();
     }
 }
