0d44ff6caa7884e1e342e522285fc75b738289f4
GapIV <danichgap@gmail.com>
2024-02-14 14:40:12 +0300

    UserRepository added

---
+++ ToDo.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.DAL.Entities;
+
+namespace ToDo.DAL.Repositories;
+
+public class UserRepository : CommonRepository<UserEntity>
+{
+    public UserRepository(ApplicationContext context) : base(context) 
+    {
+    }
+
+    public override async Task<UserEntity?> Get(Guid id)
+    {
+        return await _set.Include(u => u.ToDos).FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public override async Task<IEnumerable<UserEntity>> GetAll()
+    {
+        return await _set.Include(u => u.ToDos).ToListAsync();
+    }
+}
