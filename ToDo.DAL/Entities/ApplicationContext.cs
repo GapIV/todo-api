79537a880f981d749ea5981cf847c3af28e12f21
GapIV <danichgap@gmail.com>
2024-02-15 14:57:40 +0300

    One-to-many discribed with Fluent API

--- ToDo.DAL/Entities/ApplicationContext.cs
+++ ToDo.DAL/Entities/ApplicationContext.cs
@@ -5,8 +5,19 @@ namespace ToDo.DAL.Entities;
 public class ApplicationContext : DbContext
 {
     DbSet<ToDoEntity> ToDos { get; set; }
+    DbSet<UserEntity> Users { get; set; }
 
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {
         Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>()
+            .HasMany(u => u.ToDos)
+            .WithOne(td => td.User)
+            .HasForeignKey(td => td.UserId);
+
+        base.OnModelCreating(modelBuilder);
     }
 }
